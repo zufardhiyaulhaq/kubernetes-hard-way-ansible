8901808d67a0b6e4127325daed61ae5100a5d14d
zufardhiyaulhaq <zufardhiyaulhaq@gmail.com>
2020-09-05 09:56:51 +0200

    fix script

--- script/cluster-helper.sh
+++ script/cluster-helper.sh
@@ -12,5 +12,5 @@ echo "example 10.200.101.0/24 just input 10.200.101"
 read -p "network address: " NETWORK_ADDRESS
 
 echo ""
-find ../ -type f -exec sed -i "s/kubernetes-cluster-02/$CLUSTER_NAME/g" {} \;
-find ../ -type f -exec sed -i "s/10.200.101/$NETWORK_ADDRESS/g" {} \;
+find ../ -type f -exec sed -i "s/kubernetes-cluster-01/$CLUSTER_NAME/g" {} \;
+find ../ -type f -exec sed -i "s/10.200.100/$NETWORK_ADDRESS/g" {} \;
